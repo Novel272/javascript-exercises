55959c235dd0d6ca87ef95b6c439b1286261246e
Novel272 <mohammedalmustafak@gmail.com>
2024-08-30 05:26:52 +0400

    nice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(st,i) {
+    if(i==0){
+        return ""
+    }
+    else if(i<0){
+        return "ERROR";
+    }
+    else{
+    let stri=st;
+    for (let v=0;v<i-1;v++){
+     stri +=st;
+    }
+    return stri;
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
