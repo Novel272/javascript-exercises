9a2ee07d13d15f7665495facff66ae8e83db4cf3
Novel272 <mohammedalmustafak@gmail.com>
2024-08-31 04:03:59 +0400

    all works expect the same number multiple of times

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...ary) {
+    let ne=ary[0];
+    let v=ne.length;
+    let c=ary.length;
+    for(let z=1;z<c;z++){
+    for (let y=0;y<v;y++){
+        if (ne[y]===ary[z]){
+            let p=ne.splice(y,1);
+        }
+        else if(ne[y]===ary[z]){
+            let p=ne.splice(y,1);
+        }
+        }
+    };
+    return ne
 };
 
 // Do not edit below this line
